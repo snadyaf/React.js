75b7f71ef2ff85b780c9da36e9261cb130b85df6
unknown <snadyaandrade6045@digitalcollege.internal>
2026-07-27 22:01:27 -0300

    inclusao da aula 4

---
+++ aula 4/produtos/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ImgInicial from './components/ImgInicial/ImgInicial'
+
+function App() {
+  
+  return (
+    <>
+      <ImgInicial />
+    </>
+  )
+}
+
+export default App
---
+++ aula 4/produtos/src/components/ImgInicial/ImgInicial.jsx
@@ -0,0 +1,13 @@
+import './ImgInicial.css'
+
+export default function ImgInicial (){
+
+    return (
+
+        <section className='divImagem'>
+            <img src={'https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_1920,w_1920/7554610_CAM_LAM_DAT_ONSITE_MODULAR_SEASONAL_TOOLKIT_SS_26_FATHERS_DAY_STATEMENT_BANNER_MINI_D_2880x520_BR_f0fcc3e0c3.jpg'} alt={'dia dos pais'} className="imagemInicial"/>
+        </section>
+
+    )
+
+}
