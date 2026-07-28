687f5922515417c15b4f1675c2e2d76ee2687f4c
unknown <snadyaandrade6045@digitalcollege.internal>
2026-07-27 22:20:52 -0300

    inclusao aula 4

---
+++ aula 4/produtos/src/components/Produtos/Produtos.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import './Produtos.css'
+
+export default function Produtos (){
+
+    const [products, setProducts] = useState([]) 
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchProducts () {
+            try{
+                setLoading(true)
+                const response = await fetch('https://fakestoreapi.com/products?limit=4')
+                const data = await response.json
+                setProducts(data)
+            }catch{
+            }
+
+        }
+    })
+
+    return (
+
+        <section className='divProdutos'>
+            
+        </section>
+
+    )
+
+}
