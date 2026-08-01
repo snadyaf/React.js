cb43143a4f0f5d6ab2f05817214588a4b3a3414f
snadyaf <snadyafilo@gmail.com>
2026-08-01 14:50:42 -0300

    inclusao do component Cartoes

--- picpay-project/src/App.jsx
+++ picpay-project/src/App.jsx
@@ -8,61 +8,86 @@ import Cartoes from './components/Cartoes/Cartoes.jsx'
 import { CircleDollarSign, CreditCard, ArrowLeftRight, HandCoins, Shield, SmartphoneNfc, ChartColumn, MessageCircleMore } from "lucide-react";
 
 const servicos = [
-    { 
-      icone: <CircleDollarSign />,
-      nomeservico: "Conta Digital", 
-      descricao: "Aqui seu dinheiro rende 102% do CDI nos dias úteis"
-    }, {
-      icone: <CreditCard />,
-      nomeservico: "Cartão de Crédito", 
-      descricao: "Construa o limite que é do seu número."
-    },  {
-      icone: <ArrowLeftRight />,
-      nomeservico: "Parcele seus Pix e Boletos", 
-      descricao: "Em até 36x com toda tranquilidade."
-    },  {
-      icone: <HandCoins />,
-      nomeservico: "Empréstimo", 
-      descricao: "Análise simples e dinheiro na sua conta rapidinho."
-    }
+  {
+    icone: <CircleDollarSign />,
+    nomeservico: "Conta Digital",
+    descricao: "Aqui seu dinheiro rende 102% do CDI nos dias úteis"
+  }, {
+    icone: <CreditCard />,
+    nomeservico: "Cartão de Crédito",
+    descricao: "Construa o limite que é do seu número."
+  }, {
+    icone: <ArrowLeftRight />,
+    nomeservico: "Parcele seus Pix e Boletos",
+    descricao: "Em até 36x com toda tranquilidade."
+  }, {
+    icone: <HandCoins />,
+    nomeservico: "Empréstimo",
+    descricao: "Análise simples e dinheiro na sua conta rapidinho."
+  }
 
 ]
 
-const beneficio = [{ 
-      icone: <Shield />,
-      nomebeneficio: "Modo Seguro",
-      descricao: "Proteção com múltiplas camadas de tecnologia."
+const beneficio = [{
+  icone: <Shield />,
+  nomebeneficio: "Modo Seguro",
+  descricao: "Proteção com múltiplas camadas de tecnologia."
 
-    }, {
-      icone: <SmartphoneNfc />,
-      nomebeneficio: "A Conta das Contas",
-      descricao: "Conecte suas contas de outros bancos aqui."
+}, {
+  icone: <SmartphoneNfc />,
+  nomebeneficio: "A Conta das Contas",
+  descricao: "Conecte suas contas de outros bancos aqui."
 
-    }, {
-      icone: <ChartColumn />,
-      nomebeneficio: "Assistente de Pagamentos",
-      descricao: "Receba alertas e pague boletos em um só lugar."
-    }, {
-      icone: <MessageCircleMore />, 
-      nomebeneficio: "Pix no Whatsapp",
-      descricao: "Faça Pix por áudio ou mensagem com PicPay."
-    }
-  
-  ]
+}, {
+  icone: <ChartColumn />,
+  nomebeneficio: "Assistente de Pagamentos",
+  descricao: "Receba alertas e pague boletos em um só lugar."
+}, {
+  icone: <MessageCircleMore />,
+  nomebeneficio: "Pix no Whatsapp",
+  descricao: "Faça Pix por áudio ou mensagem com PicPay."
+}
+
+]
+
+const cartoes = [{
+  nome: "Picpay Gold",
+  img: "https://publish-p153277-e1666050.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--c8f4da62-3c99-47db-a9dd-77bc26b9e055/picpay_gold_card_image.webp",
+  limite: "Limite de até R$4 mil",
+  vantagens: ["Anuidade zero", "Pix e Boletos em 24x", "Parcelamento de compras feitas à vista"]
+}, {
+  nome: "Picpay Platinum",
+  img: "https://publish-p153277-e1666050.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--37c19e1f-33b9-475b-bc99-8474c4fde4b7/picpay_platinum_card_image.webp",
+  limite: "Limite de até R$10 mil",
+  vantagens: ["0,5% de cashback que rende 102% do CDI", "Seguro médico em viagens", "Até 3 cartões adicionais"]
+}, {
+  nome: "Picpay Black",
+  img: "https://publish-p153277-e1666050.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--37c19e1f-33b9-475b-bc99-8474c4fde4b7/picpay_platinum_card_image.webp",
+  limite: "Consulte limites",
+  vantagens: ["1,2% de cashback que rende 102% do CDI", "Sala VIP em aeroportos", "Até 5 cartões adicionais"]
+}, {
+  nome: "Picpay Epic",
+  img: "https://publish-p153277-e1666050.adobeaemcloud.com…e9af-bd9d-4293-a43e-b4a91d2d6e24/cartao_epic.webp",
+  limite: "Consulte limites",
+  vantagens: ["Até 4% de cashback", "Até 3x sem juros nas compras internacionais", "Cofrinho Turbinado com rendimento diário de 121% do CDI"]
+}
+
+
+]
 
 
 
 function App() {
-  
- return ( 
- 
+
+  return (
+
     <>
 
-    <Header/>
-    <MainPrincipal/>
-    <MainServices servicos={servicos}/>
-    <Beneficios beneficios={beneficio}/>
-    <Cartoes/>
+      <Header />
+      <MainPrincipal />
+      <MainServices servicos={servicos} />
+      <Beneficios beneficios={beneficio} />
+      <Cartoes cartoes={cartoes} />
 
     </>
   )
--- picpay-project/src/components/Cartoes/Cartoes.jsx
+++ picpay-project/src/components/Cartoes/Cartoes.jsx
@@ -1,17 +1,48 @@
 import './Cartoes.css'
 
-function Cartoes() {
+function Cartoes({ cartoes }) {
     return (
 
         <>
-            <div className= "sectionCartoes">
 
-                <p>Para você</p>
-                <h1>Aproveitar mil possibilidades em um só cartão</h1>
-                <h2>Cartão de Crédito</h2>
-                <h3>Parcele suas compras em até 36x com PicPay Card</h3>
-            </div>
+            <section className="tituloCartoes">
 
+
+                <p className="txtParaVoce">Para você</p>
+
+                <div className="txtAproveitar">
+                    <h1>Aproveitar mil <br /> possibilidades <br /> em um só cartão</h1>
+                    <h3>Parcele suas compras em <br /> até 36x com PicPay Card.</h3>
+                </div>
+
+                <div className="containerCartoes">
+
+                    <h2>Cartão de Crédito</h2>
+
+                    <div className="containerCards">
+
+                        {cartoes.map((cartao) => (
+                            <div className="cardsCartoes" key={cartao.nome}>
+
+                                <h4>{cartao.nome}</h4>
+                                <div className="imgCartao">
+                                    <img src={cartao.img} alt={cartao.nome} />
+                                </div>
+                                <h4>{cartao.limite}</h4>
+                                <ul>
+                                    {cartao.vantagens.map((vantagem, index) => (
+                                        <li key={index}>{vantagem}</li>
+                                    ))}
+                                </ul>
+
+                            </div>
+                        ))}
+
+                    </div>
+
+                </div>
+
+            </section>
         </>
 
     )
