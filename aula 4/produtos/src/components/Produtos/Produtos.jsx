35ef24ffedc23554188a304fda651e7672f81e2f
snadyaf <snadyafilo@gmail.com>
2026-08-05 21:42:07 -0300

    ajustes no projeto loja

--- aula 4/produtos/src/components/Produtos/Produtos.jsx
+++ aula 4/produtos/src/components/Produtos/Produtos.jsx
@@ -6,6 +6,7 @@ export default function Produtos() {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
+    const [mostrarFiltro, setMostrarFiltro] = useState(false)
 
     useEffect(() => {
         async function fetchProducts() {
@@ -22,9 +23,36 @@ export default function Produtos() {
         fetchProducts()
     }, [])
 
-    const pesquisa = products.filter(product => product.category === 'jewelery', 'mens clothing');
+    function abreFiltro() {
+        setMostrarFiltro(true)
+    }
+
+    // const pesquisa = products.filter(product => product.category === 'jewelery', 'mens clothing');
+
 
     return (
+
+
+        <>
+
+            <div className="containerFiltro">
+
+                <button onClick={abreFiltro} className="filtros">Filtrar</button>
+
+                {mostrarFiltro && (
+                <div className="popupFiltro">
+
+                    <h3>Filtrar por categoria</h3>
+                    <input type="checkbox" name="jewelery" id="jewelery" />
+                    <label htmlFor="jewelery">Jewelery</label>
+                    <input type="checkbox" name="mens clothing" id="mens clothing" />
+                    <label htmlFor="mens clothing">Mens Clothing</label>
+
+                    <button className="mostraItem">Mostrar Itens</button>
+
+       </div> )}
+
+        </div>
 
         <div className="containerCards">
 
@@ -39,7 +67,9 @@ export default function Produtos() {
 
         </div>
 
+        </>
 
-    )
+
+            )
 
 }
